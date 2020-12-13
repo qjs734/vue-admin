f0df6f9abb28c4a61e28a7b9ef1699b62a6556d9
秦佳盛 <1337275169@qq.com>
2020-12-13 15:23:15 +0800

    feat(list.vue):上传图片功能完成

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-button @click="Visible = true" type="primary" icon="el-icon-plus"
+      >添加</el-button
+    >
 
     <el-table :data="trademarkList" border style="width: 100%; margin: 20px 0">
       <el-table-column type="index" label="序号" width="80" align="center">
@@ -31,6 +33,52 @@
       :total="total"
     >
     </el-pagination>
+
+    <el-dialog title="添加品牌" :visible.sync="Visible" width="50%">
+      <el-form
+        :model="trademarkForm"
+        ref="trademarkForm"
+        label-width="100px"
+        :rules="rules"
+      >
+        <el-form-item label="品牌名称" prop="tmName">
+          <el-input v-model="trademarkForm.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" prop="logoUrl">
+          <!--
+            前提允许跨域
+              action="http://182.92.128.115/admin/product/fileUpload"
+              目标服务器地址: 代理配置中 (vue.config.js)
+
+            不允许跨域，就使用proxy
+              action="/dev-api/admin/product/fileUpload"
+              /dev-api -> request.js 代理
+             在main.js中定义 Vue.prototype.$BASE_API = process.env.VUE_APP_BASE_API
+           -->
+          <el-upload
+            class="avatar-uploader"
+            :action="`${$BASE_API}/admin/product/fileUpload`"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img
+              v-if="trademarkForm.logoUrl"
+              :src="trademarkForm.logoUrl"
+              class="avatar"
+            />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+          <span></span>只能上传jpg/png文件，且不超过50kb
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="Visible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('trademarkForm')"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -47,9 +95,70 @@ export default {
       total: 0,
       page: 1,
       limit: 3,
+      Visible: false,
+      trademarkForm: {
+        tmName: "",
+        logoUrl: "",
+      },
+      rules: {
+        tmName: [
+          {
+            required: true,
+            message: "请输入品牌名称",
+            trigger: "blur",
+          },
+        ],
+        logoUrl: [
+          {
+            required: true,
+            message: "请上传品牌LOGO",
+          },
+        ],
+      },
     };
   },
   methods: {
+    //提交表单的方法
+    submitForm(form) {
+      this.$refs[form].validate(async (valid) => {
+        if (valid) {
+          //校验通过
+          // console.log(this.trademarkForm);
+          const result = await this.$API.trademark.addTrademark(
+            this.trademarkForm
+          );
+          if (result.code === 200) {
+            this.$message.success("添加品牌数据成功");
+            this.Visible = false; //隐藏对话框
+            this.getPageList(this.page, this.limit); //请求加载新数据
+          } else {
+            this.$message.error(result.message);
+          }
+        }
+      });
+    },
+    // 上传图片成功
+    handleAvatarSuccess(res) {
+      this.trademarkForm.logoUrl = res.data;
+    },
+    //上传图片之前的回调,用于验证图片格式及大小是否符合规则
+    beforeAvatarUpload(file) {
+      console.log(file);
+      const imgTypes = ["image/jpeg", "image/png", "image/jpg"];
+      //检测文件类型
+      const isValidType = imgTypes.indexOf(file.type) > -1;
+      //检查文件大小
+      const isLt = file.size / 1024 < 50;
+
+      if (!isValidType) {
+        this.$message.error("上传品牌LOGO只能是 JPG 格式或 PNG 格式!");
+      }
+      if (!isLt) {
+        this.$message.error("上传头像图片大小不能超过 50kb!");
+      }
+      //返回值为true代表可以上传，否则不可以上传
+      return isValidType && isLt;
+    },
     //改变页面显示多少
     // handleSizeChange(limit) {
     //   console.log("limit", limit);
